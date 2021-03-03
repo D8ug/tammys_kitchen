ef7cafb63a1f63be12bfb8828082fb8a99544fa6
D8ug <D8ugOfficial@gmail.com>
2021-03-03 15:05:56 +0200

    Version 0.1.1
    Just realized it didn't have the adapter in this version so I created one and fixed

--- app/src/main/java/com/schoolproject/tammyskitchen/MenuActivity.kt
+++ app/src/main/java/com/schoolproject/tammyskitchen/MenuActivity.kt
@@ -8,5 +8,26 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+
+
+    }
+
+    private fun generateDummyList(size: Int) : List<MenuItem> {
+        val list = ArrayList<MenuItem>()
+
+        for (i in 0 until size) {
+            val drawable = when (i % 5) {
+                0 -> R.drawable.pizza
+                1 -> R.drawable.pasta
+                2 -> R.drawable.hotdogs
+                3 -> R.drawable.hamburger
+                else -> R.drawable.salad
+
+            }
+
+        }
+
+        return list
     }
 }
---
+++ app/src/main/java/com/schoolproject/tammyskitchen/MenuAdapter.kt
@@ -0,0 +1,41 @@
+package com.schoolproject.tammyskitchen
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.menu_item.view.*
+
+class MenuAdapter(private val menuItemsList: List<MenuItem>) : RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuAdapter.ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.menu_item, parent, false)
+        // creates an itemView that is on the format of MenuItem, then calls ViewHolder to link the
+        // items on the itemView with values and returns the created view holder
+        return ViewHolder(itemView)
+    }
+
+
+    override fun onBindViewHolder(holder: MenuAdapter.ViewHolder, position: Int) {
+        /*
+        This function handles all the changes of the MenuItem based on the given parameters after the
+        onCreateViewHolder creates for us a ViewHolder and links all the parameters needed
+         */
+        val currentItem = menuItemsList[position]
+
+        holder.imageView.setImageResource(currentItem.imageResourse)
+        holder.itemPrice.text = "${currentItem.price}₪"
+        holder.itemName.text = currentItem.itemName
+        holder.itemDescription.text = currentItem.itemDescription
+    }
+
+    override fun getItemCount(): Int = menuItemsList.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val imageView: ImageView = itemView.image_view
+        val itemName: TextView = itemView.item_name_text_view
+        val itemDescription: TextView = itemView.description_text_view
+        val itemPrice: TextView = itemView.price_text_view
+    }
+}
